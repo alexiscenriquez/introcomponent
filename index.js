405289034b2b47f494b447846401cfbcf2d9b7f9
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-02 22:04:30 -0500

    active states

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ function validate() {
     let pwd = document.forms["claim"]["pwd"].value;
 
     if (fName == "") {
-        document.getElementById("error1").style.display = "block"
+        document.getElementById("error1").style.opacity = "1"
     document.getElementById("fName").style.border="1pt solid red"
     }
     if (lName == "") {
-        document.getElementById("error2").style.display = "block"
+        document.getElementById("error2").style.opacity = "1"
         document.getElementById("lName").style.border="1pt solid red"
     }
     if (email == "") {
-        document.getElementById("error3").style.display = "block"
+        document.getElementById("error3").style.opacity = "1"
         document.getElementById("email").style.border="1pt solid red"
     }
     if (pwd == "") {
-        document.getElementById("error4").style.display = "block"
+        document.getElementById("error4").style.opacity = "1"
         document.getElementById("pwd").style.border="1pt solid red"
      
     }
